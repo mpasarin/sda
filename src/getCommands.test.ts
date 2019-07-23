22b7ec8877c6156e56c2ccd07fbf55258ba47048
Kimberly Fraint <kifraint@microsoft.com>
2019-07-23 16:06:58 -0700

    Cleanup

--- src/getCommands.test.ts
+++ src/getCommands.test.ts
@@ -1,71 +1,71 @@
 import _ from 'lodash';
 import getCommands from '../src/getCommands';
 import Log from './Log';
-import { env as original } from './test/Constants';
+import { env as environment } from './test/Constants';
 
 beforeAll(() => {
   Log.isEnabled = false;
 });
 let env: any;
-beforeEach(() => { env = _.cloneDeep(original); });
+beforeEach(() => { env = _.cloneDeep(environment); });
 
 test('get an inline command', () => {
-  const cmd = getCommands(env, ['inlineCommand'], []);
+  const cmd = getCommands(env, ['inlineCommand']);
   expect(cmd.length).toBe(1);
   expect(cmd[0].cmd).toEqual(['inline']);
 });
 
 test('get an inline array command', () => {
-  const cmd = getCommands(env, ['inlineArrayCommand'], []);
+  const cmd = getCommands(env, ['inlineArrayCommand']);
   expect(cmd.length).toBe(1);
   expect(cmd[0].cmd).toEqual(['inline1', 'inline2']);
 });
 
 test('get a regular command', () => {
-  const cmd = getCommands(env, ['regularCommand'], []);
+  const cmd = getCommands(env, ['regularCommand']);
   expect(cmd.length).toBe(1);
   expect(cmd[0].cmd).toEqual(['regular']);
 });
 
 test('get a regular array command', () => {
-  const cmd = getCommands(env, ['regularArrayCommand'], []);
+  const cmd = getCommands(env, ['regularArrayCommand']);
   expect(cmd.length).toBe(1);
   expect(cmd[0].cmd).toEqual(['regular1', 'regular2']);
 });
 
 test('get a command with folder', () => {
-  const cmd = getCommands(env, ['commandWithFolder'], []);
+  const cmd = getCommands(env, ['commandWithFolder']);
   expect(cmd.length).toBe(1);
   expect(cmd[0].cmd).toEqual(['withFolder']);
   expect(cmd[0].cwd).toEqual('C:\\');
 });
 
 test('get a non-existing command', () => {
-  const cmd = getCommands(env, ['nonExistingCommand'], []);
+  const cmd = getCommands(env, ['nonExistingCommand']);
   expect(cmd.length).toBe(0);
 });
 
 test('get two commands', () => {
-  const cmd = getCommands(env, ['inlineCommand', 'regularCommand'], []);
+  const cmd = getCommands(env, ['inlineCommand', 'regularCommand']);
   expect(cmd.length).toBe(2);
   expect(cmd[0].cmd).toEqual(['inline']);
   expect(cmd[1].cmd).toEqual(['regular']);
 });
 
 test('get an existing and a non-existing command', () => {
-  const cmd = getCommands(env, ['inlineCommand', 'nonExistingCommand'], []);
+  const cmd = getCommands(env, ['inlineCommand', 'nonExistingCommand']);
   expect(cmd.length).toBe(1);
   expect(cmd[0].cmd).toEqual(['inline']);
 });
 
 test('get a non-existing and an existing command', () => {
-  const cmd = getCommands(env, ['nonExistingCommand', 'inlineCommand'], []);
+  const cmd = getCommands(env, ['nonExistingCommand', 'inlineCommand']);
   expect(cmd.length).toBe(1);
   expect(cmd[0].cmd).toEqual(['inline']);
 });
 
 test('get a command with a valid param', () => {
-  const cmd = getCommands(env, ['commandWithParams'], [['-param', 'paramValue'], ['-wtf']]);
+  const cmd = getCommands(env, ['commandWithParams'], [['-param', 'paramValue'], ['-invalidParam']]);
   expect(cmd.length).toBe(1);
   expect(cmd[0].cmd).toEqual(['withParams -param paramValue']);
 });
--- src/getCommands.ts
+++ src/getCommands.ts
@@ -3,7 +3,11 @@ import { ICommand, IEnvironment, ITemplate } from './interfaces';
 import { IConfigCommand } from './interfaces/IConfig';
 import Log from './Log';
 
-export default function getCommands(environment: IEnvironment, commandNames: string[], params: string[][]): ICommand[] {
+export default function getCommands(
+  environment: IEnvironment,
+  commandNames: string[],
+  params?: string[][]
+): ICommand[] {
   const cmds: ICommand[] = [];
   for (const cmdName of commandNames) {
     const cmd = getCommand(environment.template, cmdName, params);
@@ -14,7 +18,7 @@ export default function getCommands(environment: IEnvironment, commandNames: str
   return cmds;
 }
 
-function getCommand(template: ITemplate, cmdName: string, params: string[][]): ICommand | undefined {
+function getCommand(template: ITemplate, cmdName: string, params?: string[][]): ICommand | undefined {
   let command = template.commands[cmdName];
   if (!command) {
     Log.error(`Command "${cmdName}" not found in template "${template.id}"`);
@@ -27,7 +31,7 @@ function getCommand(template: ITemplate, cmdName: string, params: string[][]): I
   } else if (isArray(command)) {
     command = { cmd: command };
   } else if (isString(command.cmd)) {
-    const paramString = getParamString(command, params);
+    const paramString = params ? getParamString(command, params) : '';
     command.cmd = addParams(command.cmd, paramString);
     command.cmd = [command.cmd];
   }

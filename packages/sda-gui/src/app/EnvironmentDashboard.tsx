11b28668bce3babcbcf670a3612867746ed17f0c
Marc Pasarin <mpasarin@microsoft.com>
2020-02-28 13:55:49 -0800

    Move template to the additional information area

--- packages/sda-gui/src/app/EnvironmentDashboard.tsx
+++ packages/sda-gui/src/app/EnvironmentDashboard.tsx
@@ -19,20 +19,27 @@ export default class EnvironmentDashboard extends React.Component<IEnvironmentDa
     if (!this.props.env) { return null; }
     const env = this.props.env;
     return (<div style={{ margin: '10px', flexGrow: 1 }}>
-      <h1>{env.id} ({env.templateId})</h1>
-      <ul style={{ listStyle: 'none' }}>
-        <li><Link href='#' onClick={() => exec(`start ${env.path}`)}>
-          <Icon iconName='FolderHorizontal' /> {env.path}
-        </Link></li>
-        {env.template.gitRepo
-          ? (<div>
-            <li><Link href='#' onClick={() => exec(`start ${env.template.gitRepo}`)}>
-              <Icon iconName='GitGraph' /> {env.template.gitRepo}
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <div>
+          <h1 style={{ margin: '20px', fontSize: '2.5em' }}>{env.id}</h1>
+        </div>
+        <div>
+          <ul style={{ listStyle: 'none' }}>
+            <li>Template: {env.templateId}</li>
+            <li><Link href='#' onClick={() => exec(`start ${env.path}`)}>
+              <Icon iconName='FolderHorizontal' /> {env.path}
             </Link></li>
-            {env.branchName ? <li>Branch: {env.branchName}</li> : undefined}
-          </div>)
-          : null}
-      </ul>
+            {env.template.gitRepo
+              ? (<div>
+                <li><Link href='#' onClick={() => exec(`start ${env.template.gitRepo}`)}>
+                  <Icon iconName='GitGraph' /> {env.template.gitRepo}
+                </Link></li>
+                {env.branchName ? <li>Branch: {env.branchName}</li> : undefined}
+              </div>)
+              : null}
+          </ul>
+        </div>
+      </div>
       <hr />
       <h2>Commands</h2>
       <CommandsList env={env} />
